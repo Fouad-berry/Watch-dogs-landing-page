178fbd56e728984dbd8724ceb8f5c4f92458311f
Fouad-berry <fidriss96@gmail.com>
2024-06-11 22:15:54 +0200

    add fontstyle

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,15 @@ import { NavBar } from '../MyNavBar/NavBar';
 import '../../assets/fonts/fonts.css';
 import './Home.css';
 
-
 export const Home = () => {
     return (
-        <div className="image-container">
-            <NavBar/>
-            <img src={banner} alt="L'équipe de Watch Dogs" className="img-fluid" />
-            <h1>WATCH DOGS</h1>
+        <div className="home-container">
+            <div className="image-container">
+                <img src={banner} alt="L'équipe de Watch Dogs" className="img-fluid" />
+                <div className="text-overlay">
+                    <h1>WATC<span className='high'>H</span> DOGS</h1>
+                </div>
+            </div>
         </div>
     );
 };
